cee0538bafd4122b465fa65b4e5b29d36c3a09f7
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2019-12-06 13:20:15 +0100

    :building_construction: Configure routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import { Grommet, Box, Heading, ThemeValue } from 'grommet';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { Welcome } from './screens';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <p>Group Communication Survey</p>
-    </div>
+    <Grommet theme={grommetTheme} full={true}>
+      <Box pad="small" background="black">
+        <Heading level="3" margin="small">
+          Group Communication Survey
+        </Heading>
+      </Box>
+
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Welcome} />
+          <Route component={() => <Redirect to="/" />} />
+        </Switch>
+      </BrowserRouter>
+    </Grommet>
   );
+};
+
+const grommetTheme: ThemeValue = {
+  global: {
+    font: {
+      family: 'Fira Sans',
+    },
+  },
 };
 
 export default App;
---
+++ src/screens/Welcome.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Heading } from 'grommet';
+
+const Welcome: React.FC = () => {
+  return (
+    <Heading level="4" margin="medium">
+      Welcome
+    </Heading>
+  );
+};
+
+export default Welcome;
