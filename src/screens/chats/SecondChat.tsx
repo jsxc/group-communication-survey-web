d0299a14d6ca5ac02fca2ace7358ad1b7ab5f570
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2020-01-31 14:29:24 +0100

    :sparkles: Build /chat-2 page

--- src/screens/chats/SecondChat.tsx
+++ src/screens/chats/SecondChat.tsx
@@ -9,68 +9,75 @@ const SecondChat: React.FC = () => {
 
   const messages = [
     {
-      text: 'Did you watch the game yesterday?',
+      text: 'Hey guys can anyone take me tomorrow?',
       createdAt: new Date('2020-01-30T20:00:00'),
+      user: Arthur,
+    },
+    {
+      text: "What's wrong?",
+      createdAt: new Date('2020-01-30T20:01:00'),
       user: Karl,
     },
     {
-      text: 'One or more messages are missing',
-      createdAt: new Date('2020-01-30T20:01:00'),
-      user: null,
-      isSystemMessage: true,
+      text: "You actually don't miss a chance to pose with your cart ...",
+      createdAt: new Date('2020-01-30T20:02:00'),
+      user: Karl,
     },
     {
-      text: '👍',
-      createdAt: new Date('2020-01-30T20:02:00'),
+      text: 'Everything alright?',
+      createdAt: new Date('2020-01-30T20:03:00'),
       user: Lara,
     },
     {
-      text: 'One or more messages are missing',
-      createdAt: new Date('2020-01-30T20:03:00'),
-      user: null,
-      isSystemMessage: true,
-    },
-    {
-      text: 'Was out yesterday',
+      text: 'Well, stupid story 😂',
       createdAt: new Date('2020-01-30T20:04:00'),
       user: Arthur,
     },
     {
-      text: 'That in Stuttgart',
+      text: 'A tractor and my BMW had a disagreement',
       createdAt: new Date('2020-01-30T20:05:00'),
-      user: Karl,
+      user: Arthur,
     },
     {
-      text: 'One or more messages are missing',
+      text: '???',
       createdAt: new Date('2020-01-30T20:06:00'),
-      user: null,
-      isSystemMessage: true,
+      user: Lara,
     },
     {
-      text: 'That was Hammer',
+      text: '😂 Oh dear, I feel bad',
       createdAt: new Date('2020-01-30T20:07:00'),
       user: Karl,
     },
     {
-      text: 'Visited Ingo',
+      text:
+        "He said he had to cut the curve and my parked BMW didn't want to make room",
       createdAt: new Date('2020-01-30T20:08:00'),
       user: Arthur,
-      inferenceQuote: '👍',
     },
     {
-      text: 'How is he?',
+      text: 'Do you have the other driver?',
       createdAt: new Date('2020-01-30T20:09:00'),
+      user: Karl,
+    },
+    {
+      text: 'Yup',
+      createdAt: new Date('2020-01-30T20:10:00'),
+      user: Arthur,
+    },
+    {
+      text: 'But are you okay?',
+      createdAt: new Date('2020-01-30T20:11:00'),
       user: Lara,
     },
     {
-      text: 'Yes yes',
-      createdAt: new Date('2020-01-30T20:10:00'),
-      user: Karl,
+      text: 'No',
+      createdAt: new Date('2020-01-30T20:12:00'),
+      user: Arthur,
     },
     {
-      text: 'Just ignore me',
-      createdAt: new Date('2020-01-30T20:11:00'),
-      user: Karl,
+      text: "But that's stupid",
+      createdAt: new Date('2020-01-30T20:13:00'),
+      user: Lara,
     },
   ];
 
